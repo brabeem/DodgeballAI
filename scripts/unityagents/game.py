336e49044535f5f65312a2d92a73391495b963e5
brabeem <brabeem123@gmail.com>
2022-02-06 09:23:37 +0545

    250 episodes trained

--- scripts/unityagents/game.py
+++ scripts/unityagents/game.py
@@ -27,47 +27,51 @@ if __name__ == '__main__':
     n_actions = 5
     maddpg_agents = MADDPG(actor_dims, critic_dims, n_agents, n_actions, 
                            fc1=128, fc2=64,  
-                           alpha=0.001, beta=0.001, scenario=scenario,
+                           alpha=0.01, beta=0.001, scenario=scenario,
                            chkpt_dir='tmp/maddpg/')
 
     memory = MultiAgentReplayBuffer(50000, critic_dims, actor_dims, 
                         n_actions, n_agents, batch_size=1024)
 
-    N_GAMES = 10000
+    N_GAMES = 1000
     MAX_STEPS = 5000
     total_steps = 0
     score_history = []
-    evaluate = True
+    evaluate = False
     maddpg_agents.load_checkpoint()
 
     for i in range(N_GAMES):
         obs = env.reset()
-        score = 0
+        score = np.array([0,0,0,0,0,0])
         done = [False]*n_agents
         episode_step = 0
         while not any(done):
             actions = maddpg_agents.choose_action(obs)
             obs_, reward, done = env.step(actions)
-            writer.writerow(reward)
             state = obs_list_to_state_vector(obs)
             state_ = obs_list_to_state_vector(obs_)
 
             if episode_step >= MAX_STEPS:
                 done = [True]*n_agents
-
-            memory.store_transition(obs, state, actions, reward, obs_, state_, done)
-
+            if 2.0 in reward:
+                print("experience multiplied")
+                for j in range(4000):
+                    memory.store_transition(obs, state, actions, reward, obs_, state_, done)
+                    # print(reward)
             if total_steps % 100 == 0 and not evaluate:
                 maddpg_agents.learn(memory)
 
             obs = obs_
-
-            score += sum(reward)
+            score = score + np.array(reward)
+            # score += sum(reward)
             total_steps += 1
             episode_step += 1
-
+        # print("step:",episode_step)
         score_history.append(score)
-        avg_score = np.mean(score_history[-100:])
+        latest_score_history = np.array(score_history[-5:])
+        avg_score = np.mean(latest_score_history,axis=0,keepdims=False)
+        writer.writerow(avg_score)
+
         if not evaluate:
             if i%5 == 0:
                 maddpg_agents.save_checkpoint()
